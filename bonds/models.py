e35606019dd97fd2eace94b9a76b7e6c248f334a
WIN11 <jata4321@gmail.com>
2025-09-10 10:30:44 +0200

    Add initial migration, models, and cleanup unused views in bonds app

--- bonds/models.py
+++ bonds/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+from django.core.exceptions import ValidationError
 import QuantLib as ql
 
 # Create your models here.
@@ -7,7 +9,7 @@ class Bond(models.Model):
     ISIN = models.CharField(max_length=12)
     name = models.CharField(max_length=100)
     issue_price = models.DecimalField(max_digits=10, decimal_places=4)
-    issue_date = models.DateField()
+    issue_date = models.DateField(default=timezone.now)
     maturity_date = models.DateField()
     coupon_rate = models.DecimalField(max_digits=5, decimal_places=3)
     quantity = models.IntegerField()
@@ -15,11 +17,16 @@ class Bond(models.Model):
     def __str__(self):
         return self.name
 
+    def clean(self):
+        if self.issue_date >= self.maturity_date:
+            raise ValidationError('Issue date must be earlier than maturity date.')
+
+
 class BondPrice(models.Model):
 
     calendar = ql.Poland()
     settlement_days = '2D'
-    settlement_date = calendar.advance(ql.Date.todaysDate(), ql.Period(settlement_days))
+    settlement_date = calendar.advance(ql.Date.todaysDate(), ql.Period(settlement_days)).ISO()
 
     bond = models.ForeignKey(Bond, on_delete=models.CASCADE, related_name='prices')
     price = models.DecimalField(max_digits=10, decimal_places=4)
--- bonds/views.py
+++ bonds/views.py
@@ -13,24 +13,3 @@ class HomeView(TemplateView):
 
     def get_queryset(self):
         return
-
-class AboutView(TemplateView):
-    template_name = 'bonds/about.html'
-    extra_context = {
-        'title': 'About'}
-
-    def get_queryset(self):
-        return
-
-class ListingView(ListView):
-    template_name = 'bonds/listing.html'
-    extra_context = {
-        'title': 'Listing'}
-
-class DetailView(DetailView, pk_url_kwarg='id'):
-    template_name = 'bonds/detail.html'
-    extra_context = {
-        'title': 'Detail'}
-
-    def get_queryset(self, pk_url_kwarg):
-        return Bond.objects.filter(id=pk_url_kwarg)
